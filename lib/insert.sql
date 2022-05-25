07e8ac524b77cf346435d94990975367366d221a
Rebecca Sissman <rsissman15@aol.com>
2022-05-25 15:19:04 -0400

    completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive ) VALUES ( 1, "Mr.Chocolate", 3, "M", "brown", "temperamental", 1);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive ) VALUES ( 2, "Rowdy", 4, "M", "black", "goofy", 1);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive ) VALUES ( 3, "Tabitha", 10, "F", "brown", "non-temperamental", 0);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive ) VALUES ( 4, "Sergeant Brown", 7, "M", "brown", "calm", 1);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive ) VALUES ( 5, "Melissa", 8, "F", "white", "naive", 1);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive ) VALUES ( 6, "Grinch", 9, "M", "black", "grinchy", 0);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive ) VALUES ( 7, "Wendy", 2, "F", "black", "aggressive", 1);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive ) VALUES ( 8, NULL, 13, "M", "brown", "violent", 0);
